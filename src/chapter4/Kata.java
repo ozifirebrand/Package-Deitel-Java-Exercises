198b55e09f35fcac6dda346a5bdd99c94955ecc1
Ozioma <oziomaokoroafor@gmail.com>
2021-08-05 16:17:42 +0100

    Kata Price List

--- src/chapter4/Kata.java
+++ src/chapter4/Kata.java
@@ -9,21 +9,86 @@ The system displays factorial of the number is output.
  */
 
 
+        /* method takes in the number of units to be bought
+
+prices are cheaper for resellers
+if it's 1-4 its 1500 per copy
+5-9 its 1400
+10-29 1200
+30-49 1100
+50-99 1000
+100-199 900
+200-above 800
+
+Display total sum based on unit price * total
+ */
+
+
 public class Kata {
-    private int factorialValue=1;
-    private int counter = 0;
-
-    public int findFactorial(int input) {
-
-        while ( counter <= (input+1) ){
-            factorialValue = factorialValue * input;
-            input -= 1;
-            counter += 1;
+    int unitPrice = 1500;
+    int totalPriceOfPastQuestions;
+    public int getPrice(int numberOfPqs) {
+        boolean numberOfPqsLessThan5 = numberOfPqs>=1 && numberOfPqs <=5;
+        if (numberOfPqsLessThan5) {
+            unitPrice = 1500;
         }
-        return factorialValue;
+        boolean numberOfPqsLessThan10 = numberOfPqs>=5 && numberOfPqs <=9;
+        if (numberOfPqsLessThan10){
+            unitPrice = 1400;
+        }
 
+        boolean numberOfPqsLessThan30 =numberOfPqs>=10 && numberOfPqs <=29;
+        if (numberOfPqsLessThan30){
+            unitPrice = 1200;
+        }
+        boolean numberOfPqsLessThan50 =numberOfPqs>=30 && numberOfPqs <=49;
+        if (numberOfPqsLessThan50){
+            unitPrice = 1100;
+        }
+
+        boolean numberOfPqsLessThan100 =numberOfPqs>=50 && numberOfPqs <=99;
+        if (numberOfPqsLessThan100){
+            unitPrice = 1000;
+        }
+
+        boolean numberOfPqsLessThan200 =numberOfPqs>=100 && numberOfPqs <=199;
+        if (numberOfPqsLessThan200){
+            unitPrice = 900;
+        }
+
+        boolean numberOfPqsGreaterThan200 =numberOfPqs>=200;
+        if (numberOfPqsGreaterThan200){
+            unitPrice = 800;
+        }
+
+      totalPriceOfPastQuestions = numberOfPqs * unitPrice;
+        return totalPriceOfPastQuestions;
+
+    }
+
+    public static void main(String[] args) {
+        Kata kata = new Kata();
+        System.out.println(kata.getPrice(59));
+
+    }
 }
-}
+
+
+
+//    private int factorialValue=1;
+//    private int counter = 0;
+//
+//    public int findFactorial(int input) {
+//
+//        while ( counter <= (input+1) ){
+//            factorialValue = factorialValue * input;
+//            input -= 1;
+//            counter += 1;
+//        }
+//        return factorialValue;
+
+//}
+
 //
 //    public int returnFactorial(){
 //        return factorialValue;
--- test/chapter4/KataTest.java
+++ test/chapter4/KataTest.java
@@ -8,11 +8,72 @@ import static org.junit.jupiter.api.Assertions.*;
 class KataTest {
 
     @Test
-    public void testFactorial(){
+public void testPriceForLessThan5(){
         Kata kata = new Kata();
-       int result = kata.findFactorial(10);
-       Assertions.assertEquals(151200, result);
+        int totalPriceOfPqs = kata.getPrice(2);
+        Assertions.assertEquals(3000, totalPriceOfPqs);
 
     }
+
+    @Test
+    public void testPriceForLessThan10One(){
+        Kata kata = new Kata();
+        int totalPriceOfPqs = kata.getPrice(5);
+        Assertions.assertEquals(7000, totalPriceOfPqs);
+
+    }
+    @Test
+    public void testPriceForLessThan10(){
+        Kata kata = new Kata();
+        int totalPriceOfPqs = kata.getPrice(9);
+        Assertions.assertEquals(12600, totalPriceOfPqs);
+
+    }
+
+    @Test
+    public void testPriceForLessThan30(){
+        Kata kata = new Kata();
+        int totalPriceOfPqs = kata.getPrice(29);
+        Assertions.assertEquals(34800, totalPriceOfPqs);
+
+    }
+
+    @Test
+    public void testPriceForLessThan50(){
+        Kata kata = new Kata();
+        int totalPriceOfPqs = kata.getPrice(49);
+        Assertions.assertEquals(53900, totalPriceOfPqs);
+
+    }
+
+    @Test
+    public void testPriceForLessThan100(){
+        Kata kata = new Kata();
+        int totalPriceOfPqs = kata.getPrice(99);
+        Assertions.assertEquals(99000, totalPriceOfPqs);
+
+    }
+
+    @Test
+    public void testPriceForLessThan200(){
+        Kata kata = new Kata();
+        int totalPriceOfPqs = kata.getPrice(199);
+        Assertions.assertEquals(179100, totalPriceOfPqs);
+
+    }
+
+    @Test
+    public void testPriceForGreaterThan200(){
+        Kata kata = new Kata();
+        int totalPriceOfPqs = kata.getPrice(2000);
+        Assertions.assertEquals(1600000, totalPriceOfPqs);
+
+    }
+//    public void testFactorial(){
+//        Kata kata = new Kata();
+//       int result = kata.findFactorial(10);
+//       Assertions.assertEquals(151200, result);
+//
+//    }
 
 }
