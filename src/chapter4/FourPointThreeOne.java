89ebcccf281e7babde51ad033bc4cdd5f6eafe07
Ozioma <oziomaokoroafor@gmail.com>
2021-08-08 20:38:23 +0100

    Binary

--- src/chapter4/FourPointThreeOne.java
+++ src/chapter4/FourPointThreeOne.java
@@ -1,7 +1,8 @@
+package chapter4;
+import java.util.Scanner;
 /*user inputs number
 Number is looped by dividing by 10^n where n = 0 till remainder
     equals 0
-Save digits in a string
 Modulus digits with 10^n in a reverse order with n incrementing
     by 1 till remainder is less than 0
 Each digit is then multiplied by 2^n where n = 0 and it increments
@@ -10,23 +11,29 @@ Sum of digits gives the decimal figure
 Display sum of digits
  */
 
-package chapter4;
-import java.text.MessageFormat;
-import java.util.Scanner;
 public class FourPointThreeOne {
     public static void main(String[] args) {
 
-        int counter = 0 ;
-        int firstConversion= 0;
-        String digitsStored= "" + firstConversion;
-        int decimalNumber;
         Scanner scanner = new Scanner(System.in);
+
+        int counter = 0;
+        int firstConversion= 1;
+        int decimalNumber= 10;
+        int tens= 1;
+
         System.out.println("Enter number --> ");
-       int  binaryInteger = scanner.nextInt();
-       while(binaryInteger>= 0) {
-           firstConversion = (int) (binaryInteger / Math.pow(10, counter));
-           counter += 1;
-           digitsStored = MessageFormat.format("{0}{1}", digitsStored, firstConversion);
-       }
+        int  binaryInput = scanner.nextInt();
+
+        while (tens >= 1){
+            int binaryInteger = binaryInput/tens;
+
+
+
+        }
+
+
+
+        System.out.print("" +1255/1000 +"\n" + 1255%1000 +"\n" +1255%100) ;
+
     }
 }
