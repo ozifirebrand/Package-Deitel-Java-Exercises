fa703bdc6887df50de8db444a938b4aef3ef6edf
Ozioma <oziomaokoroafor@gmail.com>
2021-08-08 20:17:30 +0100

    Asterisks commit 2

--- src/chapter4/FourPointTwoNine.java
+++ src/chapter4/FourPointTwoNine.java
@@ -3,14 +3,19 @@ import  java.util.Scanner;
 public class FourPointTwoNine {
     public static void main(String[] args) {
         Scanner scanner  = new Scanner(System.in);
+
         System.out.println("Enter size of square here --> ");
         int squareSide = scanner.nextInt();
+
         String asterisk = "*";
-        int noOfAsterisks = 0;
-        while (noOfAsterisks <= 20){
-            noOfAsterisks = squareSide;
+
+        int noOfAsterisks = 1;
+        while (noOfAsterisks <= squareSide){
+            if (squareSide<=20){
             System.out.print(asterisk);
-            noOfAsterisks++;
+
+            noOfAsterisks++;}
+
         }
 
     }
