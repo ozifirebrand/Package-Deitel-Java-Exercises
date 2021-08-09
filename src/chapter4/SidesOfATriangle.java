c0f204b25e32ca5ac795f904c7677fc7f7167035
Ozioma <oziomaokoroafor@gmail.com>
2021-08-09 09:09:50 +0100

    Side of a triangle

---
+++ src/chapter4/SidesOfATriangle.java
@@ -0,0 +1,23 @@
+/*How to determine the third side
+
+ */
+package chapter4;
+import java.util.Scanner;
+
+public class SidesOfATriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first side -> ");
+        double firstSide = scanner.nextDouble();
+
+        System.out.println("Enter the second side -> ");
+        double secondSide = scanner.nextDouble();
+
+        System.out.println("Enter the third side -> ");
+        double thirdSide = scanner.nextDouble();
+
+
+
+    }
+}
