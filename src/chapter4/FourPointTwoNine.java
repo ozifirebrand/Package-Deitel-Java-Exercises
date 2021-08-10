a3f9e09a1947c3e00743242bf0034a39cdde539b
Ozioma <oziomaokoroafor@gmail.com>
2021-08-10 23:47:35 +0100

    Squares and loops

--- src/chapter4/FourPointTwoNine.java
+++ src/chapter4/FourPointTwoNine.java
@@ -3,8 +3,8 @@ import  java.util.Scanner;
 public class FourPointTwoNine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter size of square here --> ");
+        for (int counterNo = 1; counterNo <= 10; counterNo++) {
+        System.out.println("\nEnter size of square here --> ");
         int squareSide = scanner.nextInt();
 
         for (int column = 1; column <= squareSide; column++) {
@@ -12,20 +12,23 @@ public class FourPointTwoNine {
         }
         System.out.println();
 
-        for (int row = 0; row < squareSide-2; row++) {
-            for (int column = 0; column < 1; column++) {
-                System.out.print("*");
+            for (int row = 0; row < squareSide - 2; row++) {
+                for (int column = 0; column < 1; column++) {
+                    System.out.print("*");
+                }
+                for (int column = 1; column < squareSide; column++) {
+                    System.out.print("   ");
+                }
+                for (int column = 0; column < 1; column++) {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            for (int column = 1; column < squareSide; column++) {
-                System.out.print("   ");
+            for (int column = 1; column <= squareSide; column++) {
+                System.out.print("*  ");
             }
-            for (int column = 0; column < 1; column++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for (int column = 1; column <= squareSide; column++) {
-            System.out.print("*  ");
+
+            System.out.print("\t\t");
         }
     }
 }
