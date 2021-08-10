6751d5ba29bda5d3eba9486459141f2f59824364
Ozioma <oziomaokoroafor@gmail.com>
2021-08-10 22:07:38 +0100

    Squares and loops

--- src/chapter4/FourPointTwoNine.java
+++ src/chapter4/FourPointTwoNine.java
@@ -2,24 +2,30 @@ package chapter4;
 import  java.util.Scanner;
 public class FourPointTwoNine {
     public static void main(String[] args) {
-        Scanner scanner  = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter size of square here --> ");
         int squareSide = scanner.nextInt();
 
-        String asterisk = "*";
+        for (int column = 1; column <= squareSide; column++) {
+            System.out.print("* ");
+        }
+        System.out.println();
 
-        if (squareSide<=20){
-        for (int rows = 1;rows <= squareSide; rows++){
-            for (int column = rows; column<=squareSide; column++ ) {
-                System.out.print(asterisk);
-                // rows++;
+        for (int row = 0; row < squareSide-2; row++) {
+            for (int column = 0; column < 1; column++) {
+                System.out.print("* ");
+            }
+            for (int column = 1; column < squareSide; column++) {
+                System.out.print("  ");
+            }
+            for (int column = 0; column < 1; column++) {
+                System.out.print(" *");
             }
             System.out.println();
-            }
-
-
         }
-
+        for (int column = 1; column <= squareSide; column++) {
+            System.out.print("* ");
+        }
     }
 }
