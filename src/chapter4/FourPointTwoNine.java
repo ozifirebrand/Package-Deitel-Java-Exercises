ed0bfba4530e9e14bfa219d48bd15ee477673317
Ozioma <oziomaokoroafor@gmail.com>
2021-08-12 13:17:33 +0100

    Refactoring squares

--- src/chapter4/FourPointTwoNine.java
+++ src/chapter4/FourPointTwoNine.java
@@ -7,10 +7,13 @@ public class FourPointTwoNine {
         System.out.println("\nEnter size of square here --> ");
         int squareSide = scanner.nextInt();
 
-        for (int column = 1; column <= squareSide; column++) {
-            System.out.print("*  ");
-        }
-        System.out.println();
+        if(squareSide==1){
+            System.out.println("*");
+        }else {
+            for (int column = 1; column <= squareSide; column++) {
+                System.out.print("*  ");
+            }
+            System.out.println();
 
             for (int row = 0; row < squareSide - 2; row++) {
                 for (int column = 0; column < 1; column++) {
@@ -27,7 +30,7 @@ public class FourPointTwoNine {
             for (int column = 1; column <= squareSide; column++) {
                 System.out.print("*  ");
             }
-
+        }
             System.out.print("\t\t");
         }
     }
