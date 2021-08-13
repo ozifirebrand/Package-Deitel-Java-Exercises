f33cc9a033156fae372f29fd9011378b9bdd220f
Ozioma <oziomaokoroafor@gmail.com>
2021-08-13 13:55:36 +0100

    Multiplication of integers from one to fifteen

---
+++ src/chapter5/FourPointOneTwo.java
@@ -0,0 +1,21 @@
+package chapter5;
+/*Loops starts from one and is multiplied by a new value initialised to one
+Display shows the first output
+Loop increases by 2
+New loop value is multiplied by previous loop value up till the 15th loop
+ */
+
+public class FourPointOneTwo {
+    public static void main(String[] args) {
+        int noOfLoops =1;
+        int newNoOfLoops =1;
+        while (noOfLoops <= 15){
+            newNoOfLoops *= noOfLoops;
+
+            noOfLoops+=2;
+            //System.out.println(noOfLoops);
+        }
+        System.out.println("Multiplication of the numbers from 1 to 15 is" +
+                "\n"+newNoOfLoops);
+    }
+}
