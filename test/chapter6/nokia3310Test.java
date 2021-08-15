3799ebb508aee440de31c68c46d2c7a2606fa609
Ozioma <oziomaokoroafor@gmail.com>
2021-08-15 22:24:16 +0100

    Practicing static methods in Java

---
+++ test/chapter6/nokia3310Test.java
@@ -0,0 +1,12 @@
+package chapter6;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class nokia3310Test {
+    @Test
+    public void testThatMenuDisplaysItems(){
+        nokia3310 phone = new nokia3310();
+        //phone.menu();
+       // Assertions.assertEquals( phone.menu());
+    }
+}
