274a9501faa276433767896109be767a95d6d98c
Ozioma <oziomaokoroafor@gmail.com>
2021-08-18 23:18:18 +0100

    Multiplier for odd nos btw 1 and 15 commit

---
+++ src/chapter5/FivePointOneTwo.java
@@ -0,0 +1,19 @@
+package chapter5;
+/*
+Program prints the product of the odd numbers from one to fifteen
+Loop starter is 1. Loop continues as long as starter variable is not more than fifteen
+Each obtained number in the loop is multiplied by an initial variable one which keeps incrementing
+as loop continues.
+The starter variable increments by two after every loop completes
+ */
+public class FivePointOneTwo {
+    public static void main(String[] args) {
+        int multiplier = 1;
+        for (int counterBtw1And15 = 1; counterBtw1And15 <= 15; counterBtw1And15+=2){
+            multiplier *= counterBtw1And15;
+        }
+        System.out.println(multiplier);
+
+
+    }
+}
