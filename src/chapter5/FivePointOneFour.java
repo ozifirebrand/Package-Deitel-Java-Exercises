cd06dccac5aca4633d26c789e99ce4bc1eff34b3
Ozioma <oziomaokoroafor@gmail.com>
2021-08-19 00:20:10 +0100

    Compound interest commit refactored

--- src/chapter5/FivePointOneFour.java
+++ src/chapter5/FivePointOneFour.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 After computation, the rate increases by one percent and the interest is calculated again
 This continues till interest rises to 10 percent
  */
-public class CompoundInterest {
+public class FivePointOneFour {
     double amount;
     public void principalCalculator(){
         java.util.Scanner scanner = new Scanner(System.in);
@@ -21,9 +21,29 @@ public class CompoundInterest {
         System.out.printf("Amount in %d years is %.2f Naira", time, amount);
     }
 
+   public  void increasingRate(int rate){
+
+              Scanner scanner = new Scanner(System.in);
+               System.out.println("P: ");
+               double principal = scanner.nextDouble();
+               System.out.println("T");
+               int time = scanner.nextInt();
+       while (rate <=10){
+               amount = principal * Math.pow((1 + rate/100.0), time);
+
+               System.out.printf("Amount in %d years is %.2f Naira at rate %.2f%%", time, amount, rate/100.0);
+
+               rate++;
+               System.out.println();
+           }
+       }
+
+
     public static void main(String[] args) {
-        CompoundInterest interest = new CompoundInterest();
-        interest.principalCalculator();
+        FivePointOneFour interest = new FivePointOneFour();
+        //interest.principalCalculator();
+        interest.increasingRate(5);
+
     }
 //amount = principal (1 + r)^t
 }
