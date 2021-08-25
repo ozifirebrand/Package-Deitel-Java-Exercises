e3c6fa06049f5eb570ad5b63aa4518855525b0f7
Ozioma <oziomaokoroafor@gmail.com>
2021-08-25 22:13:36 +0100

    Chapter 7 exercises commit

--- src/ClassWork/StudentsArray.java
+++ src/ClassWork/StudentsArray.java
@@ -3,6 +3,26 @@ package ClassWork;
 import java.util.Scanner;
 
 public class StudentsArray {
+    public static void receiveInput(){
+        System.out.println("How many students and courses respectively?");
+        Scanner scanner = new Scanner(System.in);
+        int noOfStudents = scanner.nextInt();
+        int noOfCourses = scanner.nextInt();
+        int [][] table = new int[noOfStudents][noOfCourses];
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                System.out.println("Enter student" + (i+1) + "'s score for subject " + (1+j));
+                table[i][j] = scanner.nextInt();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        receiveInput();
+        //sumArrayElements(table);
+
+    }
 }
 
 
--- src/chapter7/SevenPointFour.java
+++ src/chapter7/SevenPointFour.java
@@ -1,19 +1,31 @@
 package chapter7;
 
+import java.io.OptionalDataException;
+import java.util.Scanner;
+
 public class SevenPointFour {
+    int[][] table = new int[ARRAY_SIZE][ARRAY_SIZE];
 
     public static final int ARRAY_SIZE = 3;
-    public static void main(String[] args) {
+//table.length is the number of rows in the array
 
-        int[][] table = new int[ARRAY_SIZE][ARRAY_SIZE];
-
-        //table.length is the number of rows in the array
-
-        for (int counter = 0; counter < table.length; counter++){
-            for (int index = 0; index <= ARRAY_SIZE; index++){
-                System.out.println(index);
+    public static void showArrayElements(int[][] array) {
+        for (int counter = 0; counter < array.length; counter++) {
+            for (int index = 0; index < array[counter].length; index++) {
+                System.out.print(array[counter][index]);
             }
             System.out.println();
         }
     }
+
+    public static void sumArrayElements(int[][] array) {
+        int totalOfArrays = 0;
+        for (int row = 0; row < array.length; row++) {
+            for (int column = 0; column < array[row].length; column++) {
+                totalOfArrays += array[row][column];
+            }
+            System.out.print(totalOfArrays);
+        }
+    }
+
 }
