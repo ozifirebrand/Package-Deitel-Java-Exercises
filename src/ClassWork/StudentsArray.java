359190815a156af953fb38c626333f14e51d8a14
Ozioma <oziomaokoroafor@gmail.com>
2021-08-28 19:19:43 +0100

    2D Array assignment

--- src/ClassWork/StudentsArray.java
+++ src/ClassWork/StudentsArray.java
@@ -3,7 +3,6 @@ package ClassWork;
 //import java.math.BigDecimal;
 //import java.math.RoundingMode;
 //import java.text.NumberFormat;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /* OR from an already made 2d array which is best
@@ -29,7 +28,7 @@ public class StudentsArray {
         return new int[noOfStudents][noOfCourses];
     }
 
-    private static int[][] receiveInput(){
+    private static int[][] receiveInputForEachElement(){
         Scanner scanner = new Scanner(System.in);
         int [] [] table = initialiseArray();
         for (int row = 0; row < table.length; row++) {
@@ -50,12 +49,47 @@ public class StudentsArray {
              This implies that each row will be the major factor in this computation.
              A method that adds all elements in a row and displays the sum immediately will be created.
              Another method that finds the average of the row and immediately displays it will be created.
-             These methods will be repeatedly looped until every member of the 2d array gets analysed and
-             computed.
-     */
+             These methods will be repeatedly looped in another method (hopefully) until every member
+             of the 2d array gets analysed and computed.
 
-    public static void displayTable(){
-        int [] [] table = receiveInput();
+     To display several rows simulated as tables
+        A method computes the sum for each row
+        Another is created for the average
+        Another method displays the computations side by side with the values in the row of the array
+        Another method calls these three methods (the sum, average method and the display method)
+        Another method calls this method and loops over it to display several other elements of the
+        2d array//STUCK
+     */
+    public static int determineRow(){
+        return 0;
+
+    }
+
+    private static int addInputsInOneRow(int rowNumber){
+        int[][] table = receiveInputForEachElement();
+        int totalInRow = 0;
+        for (;rowNumber< rowNumber+1;rowNumber++){
+            for ( int noOfColumns=0; noOfColumns< table[rowNumber].length; noOfColumns++){
+                totalInRow = table[rowNumber][noOfColumns];
+            }
+        }
+        System.out.print("\t" + totalInRow);
+        return totalInRow;
+    }
+
+    private static void compareSumForEachRow(){
+
+        //int sumOfRow = addInputsInOneRow(specificRow);
+        int [] [] table = receiveInputForEachElement();
+        for (int specificRow = determineRow(); specificRow < table.length; specificRow++) {
+            for (int column = 0; column < column+1; column++) {
+
+
+            }
+        }
+    }
+    public static void displayInputsInTable(){
+        int [] [] table = receiveInputForEachElement();
         for (int row = 0; row< table.length; row++){
             for (int column = 0; column < table[row].length; column++){
             System.out.println(table[row][column] + "\t");
@@ -73,7 +107,7 @@ public class StudentsArray {
         int noOfRows = 0;
         Scanner scanner = new Scanner(System.in);
         int noOfColumns = scanner.nextInt();
-        table[noOfRows] = receiveInput()[noOfColumns];
+        table[noOfRows] = receiveInputForEachElement()[noOfColumns];
         return table;
     }
 
