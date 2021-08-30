6209fa12939aac0d9de1078a7622a83cb4f06058
Ozioma <oziomaokoroafor@gmail.com>
2021-08-30 09:09:22 +0100

    Composition and inheritance assignment commit refactored

--- test/chapter7/SingingProgramTest.java
+++ test/chapter7/SingingProgramTest.java
@@ -1,4 +1,16 @@
+package chapter7;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class SingingProgramTest {
-  
+
+public class SingingProgramTest {
+
+    @Test
+    public void song(){
+        SingingProgram program = new SingingProgram();
+        program.music(5, "*");
+        Assertions.assertEquals("*", program.lyrics());
+    }
 }
