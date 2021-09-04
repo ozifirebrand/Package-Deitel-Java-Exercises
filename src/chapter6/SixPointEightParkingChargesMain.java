8b94797c750b6d19fd043377a923f915f4307e3a
Ozioma <oziomaokoroafor@gmail.com>
2021-09-04 15:26:52 +0100

    Created main class

--- src/chapter6/SixPointEightParkingChargesMain.java
+++ src/chapter6/SixPointEightParkingChargesMain.java
@@ -1,11 +1,5 @@
 package chapter6;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class SixPointEightParkingChargesTest {
-    @Test
+public class SixPointEightParkingChargesMain {
     public static void main(String[] args) {
         System.out.println(SixPointEightParkingCharges.calculateTotalAmountMadeInGarage() +" Naira");
     }
