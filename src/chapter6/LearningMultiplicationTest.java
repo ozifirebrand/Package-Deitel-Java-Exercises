afd454da66aef3312cb6b796e27f9bb34b2f192c
Ozioma <oziomaokoroafor@gmail.com>
2021-09-04 21:19:41 +0100

    Computer assisted learning - multiplication learning program refactored

--- src/chapter6/LearningMultiplicationTest.java
+++ src/chapter6/LearningMultiplicationTest.java
@@ -1,10 +1,6 @@
 package chapter6;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class LearningMultiplicationTest {
     public static void main(String[] args) {
         LearningMultiplication.confirmAnswer();
     }
-
 }
