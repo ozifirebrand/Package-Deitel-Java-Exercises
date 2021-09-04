ce77fb2f96597e8884853e5c31ebfb2f2a4125f7
Ozioma <oziomaokoroafor@gmail.com>
2021-09-04 16:14:35 +0100

    Working on other exercises commit refactored

--- src/chapter6/OtherExercisesInDeitelChapterSix.java
+++ src/chapter6/OtherExercisesInDeitelChapterSix.java
@@ -5,22 +5,28 @@ import java.util.Scanner;
 public class OtherExercisesInDeitelChapterSix {
     public static void main(String[] args) {
         calculateFloorValue();
-        calculateFloorValueToTheNearestHundreds();
-        calculateFloorValueToTheNearestTens();
+        calculateFloorValueToTheNearestTenths();
+        calculateFloorValueToTheNearestHundredths();
+        calculateFloorValueToTheNearestThousandths();
+    }
 
+    private static void calculateFloorValueToTheNearestThousandths(){
+        double x = input();
+        double y = Math.floor(x *1000 + 0.5)/ 1000;
+        System.out.println("y to the nearest thousandths is " +y);
     }
     private static double input(){
         Scanner scanner = new Scanner(System.in);
         return scanner.nextDouble();
     }
 
-    private static void calculateFloorValueToTheNearestHundreds(){
+    private static void calculateFloorValueToTheNearestHundredths(){
         double x = input();
         double y = Math.floor(x * 100 + 0.5) / 100;
         System.out.println("y to the nearest hundreds is "+y);
     }
 
-    private static void calculateFloorValueToTheNearestTens(){
+    private static void calculateFloorValueToTheNearestTenths(){
         double x = input();
         double y = Math.floor(x * 10 + 0.5) / 10;
         System.out.println("y to the nearest tens is "+y);
