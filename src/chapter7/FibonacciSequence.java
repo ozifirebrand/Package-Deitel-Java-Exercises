54b4fe2f2cf7e187914de688620bfd3af1322fcb
Ozioma <oziomaokoroafor@gmail.com>
2021-09-12 09:16:21 +0100

    fibonacci sequence second commit

--- src/chapter7/FibonacciSequence.java
+++ src/chapter7/FibonacciSequence.java
@@ -21,13 +21,10 @@ public class FibonacciSequence {
         }
         System.out.println(fibonacci);
     }
-
-
     public static void main(String[] args) {
         findFibonacci(7);
     }
 }
-
     /*User inputs a parameter into a method
     The method calculates the number's findFibonacci
         How to find the findFibonacci of each number
@@ -35,23 +32,4 @@ public class FibonacciSequence {
             This is equal to the next findFibonacci
             The new findFibonacci and the last number will be added to give the nth
             findFibonacci and so on until the nth number is reached
-
-
-          An array list saves the zeroth and first terms
-          the next findFibonacci == the sum of the last two terms.
-          add the new findFibonacci into the list and reiterate until
-          the number is equal to the index
-
-        The 0th and 1st terms are 0 and 1.
-        The 2nd findFibonacci is a sum of the zeroth and first findFibonacci
-        The 3rd findFibonacci is the sum of the second and first findFibonacci etc
-
-        int secondTerm = zerothTerm + firstTerm;
-        int thirdTerm = secondTerm + firstTerm;
-        int fourthTerm = thirdTerm + secondTerm;
-        int nthTermMinusOne = 5;
-        int nthTermMinusTwo = 2;
-        int nthTerm = nthTermMinusOne + nthTermMinusTwo;
-
-
      */
