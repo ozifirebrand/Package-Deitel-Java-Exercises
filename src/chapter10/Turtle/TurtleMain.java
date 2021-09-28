16c9432c30883c61ede49915c0816009d5bc4a69
Ozioma <oziomaokoroafor@gmail.com>
2021-09-28 09:02:11 +0100

    Turtle commit 7

--- src/chapter10/Turtle/TurtleMain.java
+++ src/chapter10/Turtle/TurtleMain.java
@@ -4,11 +4,32 @@ import chapter10.Turtle.*;
 import java.util.Scanner;
 
 public class TurtleMain {
-    Turtle turtle;
-
-    public void implementTurtle(){
+    public void implementTurtle(int input, int numberOfMoves){
         Turtle turtle = new Turtle();
-        SketchPad sketchPad = new SketchPad(20, 20)
+        SketchPad sketchPad = new SketchPad(20, 20);
+        TurtlePosition turtlePosition = new TurtlePosition(10, 10);
+        if ( input == 1 ){
+            numberOfMoves=0;
+            turtle.penUp();
+        }
+        if ( input==2 ){
+            numberOfMoves=0;
+            turtle.penDown();
+        }
+        if ( input == 3 ){
+            numberOfMoves=0;
+            turtle.turnRight();
+        }
+        if ( input==4 ){
+            numberOfMoves=0;
+            turtle.turnLeft();
+        }
+        if ( input==5){
+            turtle.writeOn(sketchPad, numberOfMoves);
+        }
+        if ( input == 6 ){
+            sketchPad.displayFloor();
+        }
     }
 
 
