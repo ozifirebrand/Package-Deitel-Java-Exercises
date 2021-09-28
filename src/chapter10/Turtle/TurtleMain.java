aa51c8cb7b3fa8eec3e7ff83da1111e66fabf360
Ozioma <oziomaokoroafor@gmail.com>
2021-09-28 08:48:54 +0100

    Turtle graphics commit 7

--- src/chapter10/Turtle/TurtleMain.java
+++ src/chapter10/Turtle/TurtleMain.java
@@ -1,26 +1,27 @@
 package chapter10.Turtle;
+import chapter10.Turtle.*;
+
+import java.util.Scanner;
 
 public class TurtleMain {
     Turtle turtle;
 
-    public PenPosition onE(){
-        return PenPosition.UP;
-    }
-
-    public PenPosition twO(){
-        return PenPosition.DOWN;
-    }
-
-    public void three(){
-        turtle.turnRight();
-    }
-
-    public void four(){
-        turtle.turnLeft();
+    public void implementTurtle(){
+        Turtle turtle = new Turtle();
+        SketchPad sketchPad = new SketchPad(20, 20)
     }
 
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+        while (input!=9){
+            if ( input == 1 ){
+
+            }
+        }
+
 
     }
 }
