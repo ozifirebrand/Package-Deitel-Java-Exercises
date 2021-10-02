10cd75d61dee5064e68c9622a42fc494cac98fa0
Ozioma <oziomaokoroafor@gmail.com>
2021-10-02 23:11:05 +0100

    Validated the input for length lesser than 13 or greater than 16

--- src/chapter10/CardValidation.java
+++ src/chapter10/CardValidation.java
@@ -4,7 +4,11 @@ public class CardValidation {
     private int cardNumberLength;
 
     public CardValidation(String number){
+        boolean numberIsLesserThan13 = number.length() < 13;
+        boolean numberIsGreaterThan16 = number.length()> 16;
+        if ( !(numberIsLesserThan13 || numberIsGreaterThan16) )
         cardNumberLength = number.length();
+        else throw new IllegalArgumentException("Invalid input");
 
     }
 
--- test/chapter10/CardValidationTest.java
+++ test/chapter10/CardValidationTest.java
@@ -12,19 +12,26 @@ class CardValidationTest {
     @DisplayName("Test that numbers cannot be lesser than 13")
     public void testLengthOfNumbersInCardNotLesserThan13(){
         //given
-        CardValidation card = new CardValidation("4537802395732");
         //when
         //assert
-        assertEquals(13, card.getSize());
+        assertThrows(IllegalArgumentException.class, ()-> new CardValidation("453780239532"));
     }
 
+
+    @Test
+    @DisplayName("Test that numbers cannot be greater than 16")
+    public void testLengthOfNumbersInCardNotGreaterThan16(){
+        //given
+        //when
+        //assert
+        assertThrows(IllegalArgumentException.class, ()-> new CardValidation("45378023957329425"));
+    }
     @Test
     @DisplayName("Test that numbers can be summed at even place from the back")
     public void testNumbersCanSumAtEvenPlace(){
         //given
-        CardValidation cardValidation = new CardValidation();
+        CardValidation cardValidation = new CardValidation("4537802395732425");
         //when
-        cardValidation.sumEvenPlacedNumbersFromBehind("3452164");
         //
 
     }
