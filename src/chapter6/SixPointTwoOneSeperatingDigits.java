b4f0f89cc6bac388b68f5d39ac6780799f0a923b
Ozioma <oziomaokoroafor@gmail.com>
2021-10-14 18:57:26 +0100

    Seperating digits program refactored

--- src/chapter6/SixPointTwoOneSeperatingDigits.java
+++ src/chapter6/SixPointTwoOneSeperatingDigits.java
@@ -7,45 +7,10 @@ public class SixPointTwoOneSeperatingDigits {
     }
 
     public static int returnRemainder(int dividend, int divisor) {
-
         return dividend%divisor;
     }
 
-    public static String displayDigits(int inputValue) {
-        int divisor = findDivisor(inputValue);
-        String valueToString = "" + inputValue;
-        int quotient;
-        int remainder;
-        String outputDisplay = "";
-        int lastIntegerIndex = 1;
-        int reducedDivisors = divisor;
-        System.out.println(reducedDivisors);
-        if ( reducedDivisors > 1 ) {
-            for (int counter = 0; counter < valueToString.length(); counter++) {
-                quotient = returnQuotient(inputValue, reducedDivisors);
-                System.out.println(quotient);
-                if ( counter == 0 ) {
-                    outputDisplay += quotient + " ";
-                } else if ( counter == valueToString.length() - 1 ) {
-                    remainder = returnRemainder(inputValue, 10 );
-                    System.out.println(remainder);
-                    outputDisplay += remainder + " ";
-                    System.out.println(outputDisplay);
-                } else {
-                    System.out.println(quotient);
-                    remainder = returnRemainder(quotient, reducedDivisors);
-                    System.out.println(remainder);
-                    outputDisplay += remainder + " ";
-                }
-                System.out.println(outputDisplay);
-                reducedDivisors /= 10;
-                System.out.println(reducedDivisors);
-            }
-        }
-        return outputDisplay;
-    }
-
-    private static int findDivisor(int digitInput) {
+    public static int findDivisor(int digitInput) {
         String inputDigitToString = "" + digitInput;
         int divisor= 1;
         for (char input : inputDigitToString.toCharArray()){
@@ -55,7 +20,22 @@ public class SixPointTwoOneSeperatingDigits {
     }
 
     public static void main(String[] args) {
-        System.out.println(displayDigits(36302));
+    }
+
+    public static String displayDigits(int inputValue) {
+        int divisor = findDivisor(inputValue);
+        String inputToString = ""+inputValue;
+        int firstSplitDigit;
+        int secondSplitDigit;
+        String splitDigit= "";
+        int ones = 1;
+        for (int counter =0; counter< inputToString.length();counter++){
+            firstSplitDigit =returnQuotient(inputValue, divisor/ones);
+            ones*= 10;
+            secondSplitDigit = returnRemainder(firstSplitDigit, 10);
+            splitDigit += secondSplitDigit+" ";
+        }
+        return splitDigit;
     }
     //fixme ASAP
 }
--- test/chapter6/SixPointTwoOneSeperatingDigitsTest.java
+++ test/chapter6/SixPointTwoOneSeperatingDigitsTest.java
@@ -6,20 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SixPointTwoOneSeperatingDigitsTest {
 
-//    @Test
-//    public void testReturnQuotient(){
-//        int returnedQuotient = SixPointTwoOneSeperatingDigits.returnQuotient(14, 3);
-//        assertEquals(4, returnedQuotient);
-//    }
-//    @Test
-//    public void testReturnRemainder(){
-//        int returnedRemainder = SixPointTwoOneSeperatingDigits.returnRemainder(14, 3);
-//        assertEquals(2, returnedRemainder);
-//    }
     @Test
-    public void testCanDisplaySplittedInput(){
-         assertEquals("5 4 6", SixPointTwoOneSeperatingDigits.displayDigits(546));
-//fixme :display works yet test is failing. Why?
+    public void testReturnQuotient() {
+        int returnedQuotient = SixPointTwoOneSeperatingDigits.returnQuotient(14, 3);
+        assertEquals(4, returnedQuotient);
     }
 
+    @Test
+    public void testReturnRemainder() {
+        int returnedRemainder = SixPointTwoOneSeperatingDigits.returnRemainder(14, 3);
+        assertEquals(2, returnedRemainder);
+    }
+
+    @Test
+    public void testFindDivisor() {
+        assertEquals(1000, SixPointTwoOneSeperatingDigits.findDivisor(2349));
+    }
+
+    @Test
+    public void testCanDisplaySplittedInput() {
+        assertEquals("5 0 9 5 4 6 ", SixPointTwoOneSeperatingDigits.displayDigits(509546));
+//fixme :display works yet test is failing. Why?
+    }
 }
