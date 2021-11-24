c3394bb5192030f1aa450e011225e2f13819c583
Ozioma <oziomaokoroafor@gmail.com>
2021-11-24 15:55:47 +0100

    Added more print statements

--- src/chapter17/ProcessEmployees.java
+++ src/chapter17/ProcessEmployees.java
@@ -1,7 +1,9 @@
 package chapter17;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class ProcessEmployees {
     public static void main(String[] args) {
@@ -17,6 +19,14 @@ public class ProcessEmployees {
         List<Employee> list = Arrays.asList(employees);
         System.out.println("Complete Employee list:");
         list.stream().forEach(System.out::println);
+        Predicate<Employee> fourToSixThousand = e->(e.getSalary()>= 4000 && e.getSalary() <= 6000);
+
+        System.out.printf("%nEmployees earning $4000-$6000 per month sorted by salary: %n");
+        list.stream().filter(fourToSixThousand).sorted(Comparator.comparing(Employee::getSalary))
+                .forEach(System.out::println);
+
+        System.out.printf("%nFirst employee who earns in the range $4000-$6000: %n%s%n",
+                list.stream().filter(fourToSixThousand).findFirst().get());
 
     }
 
