74945e1be32a5c70dd5bc25fc4171965fe911337
Ozioma <oziomaokoroafor@gmail.com>
2021-11-24 15:26:43 +0100

    Arrays and streams commit

--- src/chapter17/ArraysAndStreams.java
+++ src/chapter17/ArraysAndStreams.java
@@ -20,5 +20,25 @@ public class ArraysAndStreams {
 
         System.out.printf("Values greater than 4 (ascending with streams): %s%n",
                 greaterThan4.stream().sorted().collect(Collectors.toList()));
+
+
+        System.out.println();
+        System.out.println();
+        String[] strings ={"Red", "orange", "Yellow", "green", "Blue", "indigo", "Violet"};
+
+        System.out.printf("Original strings: %s%n", Arrays.asList(strings));
+
+        System.out.printf("strings in uppercase: %s%n", Arrays.stream(strings)
+                .map(String::toUpperCase).collect(Collectors.toList()));
+
+        System.out.printf("strings greater than m sorted ascending: %s%n",
+                Arrays.stream(strings).filter(s -> s.compareToIgnoreCase("n")<0)
+                        .sorted(String.CASE_INSENSITIVE_ORDER)
+                        .collect(Collectors.toList()));
+        System.out.printf("strings greater than m sorted descending: %s%n",
+                Arrays.stream(strings).filter(s-> s.compareToIgnoreCase("n") <0)
+                        .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                        .collect(Collectors.toList()));
+
     }
 }
