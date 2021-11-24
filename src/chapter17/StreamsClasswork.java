0d054f0131826c1156518d69229b7698bea49c96
Ozioma <oziomaokoroafor@gmail.com>
2021-11-24 10:59:57 +0100

    Class work commit

--- src/chapter17/StreamsClasswork.java
+++ src/chapter17/StreamsClasswork.java
@@ -4,6 +4,10 @@ import java.util.stream.IntStream;
 
 public class StreamsClasswork {
     public static void main(String[] args) {
-        System.out.println("Sum of numbers between 1 and 10 is "+ IntStream.range(1, 10).sum());
+        System.out.printf("Sum of numbers between 1 and 10 is %s%n", IntStream.range(1, 11).sum());
+        System.out.printf("Sum of all even numbers between 2 and 20 is %s%n",IntStream.rangeClosed(1, 10)
+                .map((int x) ->{return x *2;}).sum());
+        System.out.printf("Sum of triples of the even ints from 2 through 10 is %d%n", IntStream.
+                rangeClosed(1, 10).filter(x->x%2 ==0).map(x->x * 3).sum());
     }
 }
